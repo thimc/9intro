ecbfe53b41666e2eb319c6826751c5c3216add5f
Thim Cederlund <thim@cederlund.de>
2023-12-02 11:48:37 +0100

    chapter 3 (continued)

---
+++ 25b_biocat.c
@@ -0,0 +1,24 @@
+#include <u.h>
+#include <libc.h>
+#include <bio.h>
+
+
+void
+main(int, char*[])
+{
+	Biobuf bin;
+	Biobuf bout;
+	char* line;
+	int len;
+
+	Binit(&bin, 0, OREAD);
+	Binit(&bout, 1, OWRITE);
+	while(line = Brdline(&bin, '\n')){
+		len = Blinelen(&bin);
+		Bwrite(&bout, line, len);
+		Bflush(&bout); /* flush to standard output */
+	}
+	Bterm(&bin);
+	Bterm(&bout);
+	exits(nil);
+}
